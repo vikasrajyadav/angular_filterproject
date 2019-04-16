a0030724822a8f0ea537758aa52ce19d3a2d4421
Vikasraj Yadav <vry420@gmail.com>
2019-04-16 16:44:10 +0530

    new logic initiated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,15 @@ import { Subject } from "rxjs";
 })
 export class AppComponent {
   title = "demo-app";
-  card_datas: card_data[] = [
-    new card_data(
-      'Maths','12 std mathematics','http://www.clipartroo.com/images/7/scientist-clipart-7718.png','tag 1','science','english','me','Image',
-    ),  new card_data(
-      'English','12 std english','http://www.clipartroo.com/images/7/scientist-clipart-7718.png','tag 2','Commerce','Hindi','you','Video',
-    ),
-  ];
+  // card_datas: card_data[] = [
+  //   new card_data(
+  //     'Maths','12 std mathematics','http://www.clipartroo.com/images/7/scientist-clipart-7718.png','tag 1','science','english','me','Image',
+  //   ),  new card_data(
+  //     'English','12 std english','http://www.clipartroo.com/images/7/scientist-clipart-7718.png','tag 2','Commerce','Hindi','you','Video',
+  //   ),
+  // ];
 
-  ngOnInit(){}
+  ngOnInit() { }
 
   filterApplied: boolean = false;
   File_type = [
@@ -126,26 +126,26 @@ export class AppComponent {
     }
   ];
 
-  constructor() {}
+  constructor() { }
 
   // subject filteration starts here
 
-  subject_filter_array = [];
-  subjectName_boolean;
-  filter_value_subj = "";
+  SubjectFilterArray = [];
+  SubjectName_CheckBoxedStatus;
+  SubjectFilteredValue = "";
   subj;
 
   filterSkill = [];
 
   subjectName(subj) {
     console.log(subj.name);
-    this.subject_filter_array.push(subj.name);
-    this.filter_value_subj = subj.name;
-    console.log(this.subject_filter_array);
+    this.SubjectFilterArray.push(subj.name);
+    this.SubjectFilteredValue = subj.name;
+    // console.log(this.SubjectFilterArray);
 
     this.filterApplied = true;
 
-    if (this.subjectName_boolean) {
+    if (this.SubjectName_CheckBoxedStatus) {
       // call function
       // pushing data into the array filterskill
       this.onFilterSubjectPush();
@@ -156,20 +156,20 @@ export class AppComponent {
 
     }
 
-    console.log('before filter skill');
-    console.log(this.filterSkill);
-    console.log('after filter skill');
+    // console.log('before filter skill');
+    // console.log(this.filterSkill);
+    // console.log('after filter skill');
 
-    if(this.filterSkill.length === 0){
+    if (this.filterSkill.length === 0) {
       this.filterApplied = false;
-      console.log("reest");
+      console.log("rest");
     }
 
   }
 
   onFilterSubjectPush() {
     var data_filter = this.skill.filter(
-      element => element.name == this.filter_value_subj
+      element => element.name == this.SubjectFilteredValue
     );
 
     for (var i = 0; i < data_filter.length; i++) {
@@ -181,17 +181,17 @@ export class AppComponent {
 
   onFilterSubjectPop() {
     var data_filter_pop = this.filterSkill.filter(
-      element => element.name == this.filter_value_subj
+      element => element.name == this.SubjectFilteredValue
     );
 
-    for (var i = 0; i<this.filterSkill.length; i++) {
+    for (var i = 0; i < this.filterSkill.length; i++) {
       for (var j = 0; j < data_filter_pop.length; j++) {
 
-        if(this.filterSkill[i].id == data_filter_pop[j].id){
-          this.filterSkill.splice(i,1);
+        if (this.filterSkill[i].id == data_filter_pop[j].id) {
+          this.filterSkill.splice(i, 1);
           console.log(this.filterSkill);
           // console.log(data_filter_pop);
-        }else{
+        } else {
           break;
         }
       }
@@ -201,94 +201,12 @@ export class AppComponent {
   // subject filteration ends here
 
 
-// Author filteration starts here
-
-author_filter_array = [];
-authorName_boolean;
-filter_value_auth = "";
-auth;
-
-filterSkill2 = [];
-
-authorName(auth) {
-  // console.log("before");
-  // console.log(auth.name);
-  // console.log("after");
-  // this.author_filter_array.push(auth.name);
-  this.filter_value_auth = auth.name;
-  // // console.log(this.author_filter_array);
-
-  this.filterApplied = true;
-
-  if (this.authorName_boolean) {
-    // call function
-    // pushing data into the array filterskill
-    this.onFilterAuthorPush();
-  } else {
-    // calling the function
-    // deleting data from the array filterskill
-    this.onFilterAuthorPop();
-
-    // console.log(this.filterSkill);
-  }
-
-  // console.log('before filter2 skill');
-  // console.log('after filter2 skill');
-
-  // if(this.filterSkill.length === 0){
-  //   this.filterApplied = false;
-  //   console.log("author reset");
-  // }
-}
-
-onFilterAuthorPush() {
-  var data_filter = this.skill.filter(
-    element => element.author == this.filter_value_auth
-  );
-
-  for (var i = 0; i < data_filter.length; i++) {
+  // Author filteration starts here
 
 
-    if(this.filterSkill.length == 0){
-      this.filterSkill.push(data_filter[i]);
-      console.log("pushed");
-      console.log(this.filterSkill[i].id);
-    }
-    else{
-     if(this.filterSkill[i].name){
 
-     } else{
-
-     }
-    }
-
-  }
-  // console.log("author ARRAY");
-  // console.log(this.filterSkill2);
-}
-
-
-onFilterAuthorPop() {
-  var data_filter_pop = this.filterSkill.filter(
-    element => element.author == this.filter_value_auth
-  );
-
-  for (var i = 0; i<this.filterSkill.length; i++) {
-    for (var j = 0; j < data_filter_pop.length; j++) {
-
-      if(this.filterSkill[i].id == data_filter_pop[j].id){
-        this.filterSkill.splice(i,1);
-        console.log(this.filterSkill);
-        // console.log(data_filter_pop);
-      }else{
-        break;
-      }
-    }
-  }
-}
-
-// Author filteration ends here
-mergedArray = this.skill;
+  // Author filteration ends here
+  mergedArray = this.skill;
   // merging array
   // filter_mergedarray() {
 
@@ -300,14 +218,14 @@ mergedArray = this.skill;
   // }
 
   onUpdateSubjectName(event: Event) {
-    this.subjectName_boolean = (<HTMLInputElement>event.target).checked;
-    console.log(this.subjectName_boolean);
+    this.SubjectName_CheckBoxedStatus = (<HTMLInputElement>event.target).checked;
+    console.log(this.SubjectName_CheckBoxedStatus);
   }
 
-  onUpdateAuthorName(event: Event) {
-    this.authorName_boolean = (<HTMLInputElement>event.target).checked;
-    console.log(this.authorName_boolean);
-  }
+  // onUpdateAuthorName(event: Event) {
+  //   this.authorName_boolean = (<HTMLInputElement>event.target).checked;
+  //   console.log(this.authorName_boolean);
+  // }
 
   // function to push array in the filter skill
 }
