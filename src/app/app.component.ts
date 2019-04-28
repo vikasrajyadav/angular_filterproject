844152b1011bb5d00a1ec27e6911659b96d4b7d3
Vikasraj Yadav <vry420@gmail.com>
2019-04-28 07:44:40 +0530

    full functional filter, changes required in pop function use if function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from "@angular/core";
-import { card_data } from "./app.model";
-import { Subject } from "rxjs";
+import { Component } from '@angular/core';
+import { card_data } from './app.model';
+import { Subject } from 'rxjs';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "demo-app";
+  title = 'demo-app';
   // card_datas: card_data[] = [
   //   new card_data(
   //     'Maths','12 std mathematics','http://www.clipartroo.com/images/7/scientist-clipart-7718.png','tag 1','science','english','me','Image',
@@ -21,113 +21,114 @@ export class AppComponent {
 
   filterApplied: boolean = false;
   File_type = [
-    { name: "image", id: "1", htmlId: "image1" },
-    { name: "video", id: "2", htmlId: "video1" },
-    { name: "html", id: "3", htmlId: "html1" },
-    { name: "text", id: "4", htmlId: "text1" }
+    { name: 'image', id: '1', htmlId: 'image1' },
+    { name: 'video', id: '2', htmlId: 'video1' },
+    { name: 'html', id: '3', htmlId: 'html1' },
+    { name: 'text', id: '4', htmlId: 'text1' }
   ];
   Level = [
-    { name: "level 1", htmlId: "level1", id: "1" },
-    { name: "level 2", htmlId: "level2", id: "2" },
-    { name: "level 3", htmlId: "level3", id: "3" }
+    { name: 'level 1', htmlId: 'level1', id: '1' },
+    { name: 'level 2', htmlId: 'level2', id: '2' },
+    { name: 'level 3', htmlId: 'level3', id: '3' }
   ];
   Author = [
-    { name: "Author1", htmlId: "Author1", id: "1" },
-    { name: "Author2", htmlId: "Author2", id: "2" },
-    { name: "Author3", htmlId: "Author3", id: "3" }
+    { name: 'Author1', htmlId: 'Author1', id: '1' },
+    { name: 'Author2', htmlId: 'Author2', id: '2' },
+    { name: 'Author3', htmlId: 'Author3', id: '3' }
   ];
   Subject = [
-    { name: "Business", htmlId: "business1", id: "1" },
-    { name: "programming", htmlId: "business2", id: "2" },
-    { name: "HR", htmlId: "business3", id: "3" }
+    { name: 'Business', htmlId: 'business1', id: '1' },
+    { name: 'programming', htmlId: 'business2', id: '2' },
+    { name: 'HR', htmlId: 'business3', id: '3' }
   ];
 
   skill = [
     {
-      name: "Business",
-      author: "Author1",
-      Language: "English",
+      name: 'Business',
+      author: 'Author1',
+      Language: 'English',
       description:
-        "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
-      category: "level 1",
-      file_type: "video",
-      tag: ["tag-1", "tag-2", "tag-3"],
-      imagePath: "https://dummyimage.com/vga",
-      id: "1"
+        'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+      category: 'level 1',
+      file_type: 'video',
+      tag: ['tag-1', 'tag-2', 'tag-3'],
+      imagePath: 'https://dummyimage.com/vga',
+      id: '1'
     },
     {
-      name: "programming",
-      author: "Author1",
-      Language: "C",
-      description: "dummy 2 dummy 2 dummy 2 dummy 2 dummy 2 dummy 2 ",
-      category: "level 2",
-      file_type: "image",
-      tag: ["tag-1", "tag-2", "tag-3"],
-      imagePath: "https://dummyimage.com/vga",
-      id: "2"
+      name: 'programming',
+      author: 'Author1',
+      Language: 'C',
+      description: 'dummy 2 dummy 2 dummy 2 dummy 2 dummy 2 dummy 2 ',
+      category: 'level 2',
+      file_type: 'image',
+      tag: ['tag-1', 'tag-2', 'tag-3'],
+      imagePath: 'https://dummyimage.com/vga',
+      id: '2'
     },
     {
-      name: "programming",
-      author: "Author2",
-      Language: "C++",
-      description: "dummy 3 dummy 3 dummy 3 dummy 3 dummy 3 dummy 3 ",
-      category: "level 3",
-      file_type: "text",
-      tag: ["tag-1", "tag-2", "tag-3"],
-      imagePath: "https://dummyimage.com/vga",
-      id: "3"
+      name: 'programming',
+      author: 'Author2',
+      Language: 'C++',
+      description: 'dummy 3 dummy 3 dummy 3 dummy 3 dummy 3 dummy 3 ',
+      category: 'level 3',
+      file_type: 'text',
+      tag: ['tag-1', 'tag-2', 'tag-3'],
+      imagePath: 'https://dummyimage.com/vga',
+      id: '3'
     },
     {
-      name: "HR",
-      author: "Author2",
-      Language: "Candidate Apti",
-      description: "dummy 4 dummy 4 dummy 4 dummy 4 dummy 4 dummy 4 dummy 4 ",
-      category: "level 3",
-      file_type: "html",
-      tag: ["tag-1", "tag-2", "tag-3"],
-      imagePath: "https://dummyimage.com/vga",
-      id: "4"
+      name: 'HR',
+      author: 'Author2',
+      Language: 'Candidate Apti',
+      description: 'dummy 4 dummy 4 dummy 4 dummy 4 dummy 4 dummy 4 dummy 4 ',
+      category: 'level 3',
+      file_type: 'html',
+      tag: ['tag-1', 'tag-2', 'tag-3'],
+      imagePath: 'https://dummyimage.com/vga',
+      id: '4'
     },
     {
-      name: "Business",
-      author: "Author3",
-      Language: "hindi",
+      name: 'Business',
+      author: 'Author3',
+      Language: 'hindi',
       description:
-        "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
-      category: "level 1",
-      file_type: "video",
-      tag: ["tag-1", "tag-2", "tag-3"],
-      imagePath: "https://dummyimage.com/vga",
-      id: "5"
+        'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+      category: 'level 1',
+      file_type: 'video',
+      tag: ['tag-1', 'tag-2', 'tag-3'],
+      imagePath: 'https://dummyimage.com/vga',
+      id: '5'
     },
     {
-      name: "Business",
-      author: "Author2",
-      Language: "English",
+      name: 'Business',
+      author: 'Author2',
+      Language: 'English',
       description:
-        "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
-      category: "level 1",
-      file_type: "image",
-      tag: ["tag-1", "tag-2", "tag-3"],
-      imagePath: "https://dummyimage.com/vga",
-      id: "6"
+        'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+      category: 'level 1',
+      file_type: 'image',
+      tag: ['tag-1', 'tag-2', 'tag-3'],
+      imagePath: 'https://dummyimage.com/vga',
+      id: '6'
     },
     {
-      name: "Business",
-      author: "",
-      Language: "English",
+      name: 'Business',
+      author: '',
+      Language: 'English',
       description:
-        "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
-      category: "level 1",
-      file_type: "video",
-      tag: ["tag-1", "tag-2", "tag-3"],
-      imagePath: "https://dummyimage.com/vga",
-      id: "7"
+        'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+      category: 'level 1',
+      file_type: 'video',
+      tag: ['tag-1', 'tag-2', 'tag-3'],
+      imagePath: 'https://dummyimage.com/vga',
+      id: '7'
     }
   ];
 
   // fieldValue = ['name', 'Language', 'file_type'];
   fieldValue = [];
+  ObjectSetterValueArray = [];
 
   filterData = {
     // Language: [],
@@ -141,11 +142,11 @@ export class AppComponent {
   };
 
 
-// filterData = {
-//     Language: 'English',
-//     name: 'Business',
-//     file_type: "video",
-//   };
+  // filterData = {
+  //     Language: 'English',
+  //     name: 'Business',
+  //     file_type: "video",
+  //   };
 
 
   constructor() { }
@@ -155,23 +156,27 @@ export class AppComponent {
 
   SubjectFilterArray = [];
   SubjectName_CheckBoxedStatus;
-  SubjectFilteredValue = "";
+  SubjectFilteredValue = '';
   subj;
 
   filterSkill = [];
 
-  // tslint:disable-next-line: member-ordering
-  // sizeOfFilterData = Object.keys(this.filterData.name).length;
 
-  // tslint:disable-next-line: variable-name
-  filterDataObjectArrayCreation(filterData_CategoryNameForKey: any, FiltersubjectNameForArray: any) {
+  /*
 
-    // console.log('Size', this.sizeOfFilterData);
+  After loading the screen
+  ***********************************
+  when we select the checkbox SubjectName function will be called.
+
+  After execution of statement takes places
+
+  */
+
+  filterDataObjectArrayCreation(filterData_CategoryNameForKey: any, FiltersubjectNameForArray: any, checkboxStatus: boolean) {
+
 
     let dataValue = null;
     let flag = false;
-    // let forLoopFlag = false;
-    // tslint:disable-next-line: forin
     for (const key in this.filterData) {
       if (filterData_CategoryNameForKey === key) {
         flag = true;
@@ -180,53 +185,67 @@ export class AppComponent {
       } else {
         flag = false;
       }
-      // forLoopFlag = true;
     }
-
-    // if (forLoopFlag) {
-    // for (const key in this.filterData) {
-    if (!flag) {
-      Object.assign(this.filterData, { [filterData_CategoryNameForKey]: [] });
-      (this.filterData[filterData_CategoryNameForKey]).push(FiltersubjectNameForArray);
-      console.log(this.filterData);
+    if (checkboxStatus) {
+      if (!flag) {
+        Object.assign(this.filterData, { [filterData_CategoryNameForKey]: [] });
+        (this.filterData[filterData_CategoryNameForKey]).push(FiltersubjectNameForArray);
+        // console.log('Filter data after insertion object with array checkbox', this.filterData);
+      } else {
+        (this.filterData[dataValue]).push(FiltersubjectNameForArray);
+        // console.log('Filter data after insertion object with array checkbox', this.filterData[dataValue]);
+        console.log('true');
+      }
+      console.log('Filter data after insertion object with array checkbox', this.filterData);
+      // console.log(this.filterData);
     } else {
-      (this.filterData[dataValue]).push(FiltersubjectNameForArray);
-      console.log(this.filterData[dataValue]);
-      console.log('true');
-      // break;
+      for (let i = 0; i < this.filterData[dataValue].length; i++) {
+        if (this.filterData[dataValue][i] === FiltersubjectNameForArray) {
+          this.filterData[dataValue].splice(i, 1);
+          console.log('Filter data after Splicing object with array checkbox', this.filterData);
+          console.log(this.filterData[dataValue]);
+        }
+      }
     }
-    console.log(this.filterData);
-    // }
   }
-  //   return true;
-  // }
+
+
+  objectSetter = {
+    // name: false,
+    // Language: false,
+    // file_type: false,
+  };
 
   fieldValueCreatorFunction() {
     // const keys = Object.keys(this.filterData);
     this.fieldValue = Object.keys(this.filterData);
-    console.log(this.fieldValue);
+    console.log('FieldValue Object Data', this.fieldValue);
+    for (const ObjectSetterData of this.fieldValue) {
+      Object.assign(this.objectSetter, { [ObjectSetterData]: false });
+    }
   }
 
 
 
   subjectName(subj, cat) {
     // console.log(subj.name);
-    console.log(cat);
+    // console.log('Category name', cat);
     this.SubjectFilterArray.push(subj.name);
     this.SubjectFilteredValue = subj.name;
     // console.log(this.SubjectFilterArray);
 
-    this.filterDataObjectArrayCreation(cat, subj.name);
+    this.filterDataObjectArrayCreation(cat, subj.name, this.SubjectName_CheckBoxedStatus);
     this.fieldValueCreatorFunction();
-      // this.onFilterSubjectPush();
+    this.onFilterSubjectPush();
 
     this.filterApplied = true;
 
     if (this.SubjectName_CheckBoxedStatus) {
       // call function
       // pushing data into the array filterskill
+      // this.filterDataObjectArrayCreation(cat, subj.name, this.SubjectName_CheckBoxedStatus);
+      // this.fieldValueCreatorFunction();
       // this.onFilterSubjectPush();
-      // this.newFunction();
     } else {
       // calling the function
       // deleting data from the array filterskill
@@ -234,96 +253,94 @@ export class AppComponent {
 
     }
 
-    // console.log('before filter skill');
-    // console.log(this.filterSkill);
-    // console.log('after filter skill');
-
     if (this.filterSkill.length === 0) {
       this.filterApplied = false;
-      // console.log("reset");
     }
 
   }
 
-  // _language = ['English'];
-  // _name = ['Business'];
-
-
   onFilterSubjectPush() {
-
-    // const filter = {
-    //   Language: 'English',
-    //   name: 'Business',
-    //   file_type: "video",
-    // };
-
-
-
-    // const filter = [
-    //   ['Language', 'English', 'hindi'],
-    //   ['name', 'Business', 'programming'],
-    // ];
-
-    // for (let key of filter) {
-    //   console.log(key);
-    // }
-
-
-
     this.newArray = [];
-
-    // this.newArray = this.skill.filter(item => {
-    //   for (const value of this.fieldValue) {
-    //       // if (item[value] === filter[value]) {
-    //       //   return true;
-    //       // } else {
-    //       //   return false;
-    //     }
-    //   }
-
-
-    //   // return flag;
-    // });
-
-    // function filterFunction() {
-
-    const objectSetter = {
-      name: false,
-      Language: false,
-      file_type: false,
-    };
-
     for (const data of this.skill) {
-      objectSetter.name = false;
-      objectSetter.Language = false;
-      objectSetter.file_type = false;
+
+      // objectSetter.name = false;
+      // objectSetter.Language = false;
+      // objectSetter.file_type = false;
+
+      for (const ObjectSetterData of this.fieldValue) {
+        Object.assign(this.objectSetter, { [ObjectSetterData]: false });
+      }
 
       for (const value of this.fieldValue) {
-        console.log(value);
-        console.log(this.filterData[value]);
-        console.log(this.filterData[value].length);
+        console.log('Value', value);
+        console.log('FilterData of particular value', this.filterData[value]);
+        console.log('Length of FilterData of particular value', this.filterData[value].length);
         // tslint:disable-next-line: prefer-for-of
         for (let i = 0; i < this.filterData[value].length; i++) {
-          // console.log('filter value', filter[value][i]);
           if (data[value] === undefined || data[value] !== this.filterData[value][i]) {
             console.log('ifTrue');
-          }
-          else {
+          } else {
             console.log('ifFalse');
-            objectSetter[value] = true;
+            this.objectSetter[value] = true;
             break;
           }
         }
       }
+
+      //       let finalFlag : boolean = true;
+      //       let tempObj  = {
+      //         "a":true,
+      //         "b":true
+      //       }
+
+      //       let tempObjKey = Object.keys(tempObj); // ['a','b']
+      //       for(let tempObjKeyData in tempObj){
+      // console.log
+      //       }
+
+      // for(let objKey in this.objectSetter){
+      //   if(objKey)
+      // }
       // console.log(data);
       // for (let valueCheck of this.fieldValue) {
-      if (objectSetter.name && objectSetter.Language && objectSetter.file_type) {
+      // if (this.objectSetter.name && this.objectSetter.Language && this.objectSetter.file_type) {
+
+      // let ObjectDataValue = null;
+      let Objectflag = true;
+      this.ObjectSetterValueArray = Object.values(this.objectSetter);
+
+      // tslint:disable-next-line: forin
+      // for (const key in this.objectSetter) {
+
+      // if (this.objectSetter[key] === true) {
+      //   Objectflag = true;
+      //   // ObjectDataValue = key;
+      //   break;
+      // } else {
+      //   Objectflag = false;
+      // }
+      // }
+
+      // for(let value of tempObjKey){
+      //   if(finalFlag && value){
+      //        finalFlag = true;
+      //   }else{
+      //        finalFlag = false;
+      //   }
+      // }
+
+      for (const objectSetterValueArrayData of this.ObjectSetterValueArray) {
+        if (Objectflag && objectSetterValueArrayData) {
+          Objectflag = true;
+        } else {
+          Objectflag = false;
+        }
+      }
+
+      if (Objectflag) {
         this.newArray.push(data);
-        // }
       }
     }
-    // return flag;
-    // }
 
     console.log(this.newArray);
 
@@ -383,82 +400,11 @@ export class AppComponent {
   // subject filteration ends here
 
 
-  // Author filteration starts here
-
-
-
-  // Author filteration ends here
-  mergedArray = this.skill;
-  // merging array
-  // filter_mergedarray() {
-
-  //   for (var i = 0; i < this.filterSkill.length; i++) {
-  //     this.mergedArray.push(this.filterSkill[i]);
-  //   }
-  //   console.log(this.mergedArray);
-
-  // }
-
   onUpdateSubjectName(event: Event) {
     this.SubjectName_CheckBoxedStatus = (<HTMLInputElement>event.target).checked;
-    console.log(this.SubjectName_CheckBoxedStatus);
+    console.log('Subject Name', this.SubjectName_CheckBoxedStatus);
   }
 
-  newFunction() {
-
-    const filter = {
-      address: 'England',
-      name: 'Mark',
-      age: 25,
-    };
-    let users = [{
-      name: 'John',
-      email: 'johnson@mail.com',
-      age: 25,
-      address: 'USA'
-    },
-    {
-      name: 'Tom',
-      email: 'tom@mail.com',
-      age: 35,
-      address: 'England'
-    },
-    {
-      name: 'Mark',
-      email: 'mark@mail.com',
-      age: 28,
-      address: 'England'
-    },
-    {
-      name: 'Mark',
-      email: 'mark@mail.com',
-      age: 28,
-      address: 'England'
-    }, {
-      name: 'Mark',
-      email: 'mark@mail.com',
-      age: 28,
-      address: 'England'
-    }, {
-      name: 'Mark',
-      email: 'mark@mail.com',
-      age: 25,
-      address: 'England'
-    }
-    ];
-
-
-    users = users.filter(item => {
-      for (let key in filter) {
-        if (item[key] === undefined || item[key] != filter[key])
-          return false;
-      }
-      return true;
-    });
-
-    console.log(users);
-
-  }
 
   // onUpdateAuthorName(event: Event) {
   //   this.authorName_boolean = (<HTMLInputElement>event.target).checked;
